07e78420da1706b2ac1a20c860280a3272116af0
JuanAstudilloF <juanandresastudillo9@gmail.com>
2025-04-22 07:31:06 -0500

    Update PersonaController.java

--- src/Controllers/PersonaController.java
+++ src/Controllers/PersonaController.java
@@ -1,34 +1,42 @@
-package Models;
+package Controllers;
 
-public class Persona {
-    private String nombre;
-    private int edad;
+import Models.Persona;
 
-    public Persona(String nombre, int edad) {
-        this.nombre = nombre;
-        this.edad = edad;
+public class PersonaController {
+
+    /**
+     * Ordena un arreglo de Persona por edad utilizando el algoritmo de inserción.
+     * 
+     * @param personas Array de Persona a ordenar.
+     */
+    public void ordenarPorEdad(Persona[] personas) {
+        for (int i = 1; i < personas.length; i++) {
+            Persona actual = personas[i];
+            int j = i - 1;
+
+            // Mover elementos mayores a la edad actual hacia la derecha
+            while (j >= 0 && personas[j].getEdad() > actual.getEdad()) {
+                personas[j + 1] = personas[j];
+                j--;
+            }
+
+            personas[j + 1] = actual;
+        }
     }
 
-    @Override
-    public String toString() {
-        return nombre + " - " + edad;
-    }
-
-    // Getters
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    // Setters
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
+    /**
+     * Busca la primera persona con una edad específica.
+     * 
+     * @param personas Array donde buscar.
+     * @param edad     Edad objetivo.
+     * @return La primera Persona con la edad especificada, o null si no se encuentra.
+     */
+    public Persona buscarPorEdad(Persona[] personas, int edad) {
+        for (Persona persona : personas) {
+            if (persona.getEdad() == edad) {
+                return persona;
+            }
+        }
+        return null;
     }
 }
